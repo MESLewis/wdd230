2389e05c7246535f3a6c27346a6a9f1563940847
Morgan Lewis <MESLewis@gmail.com>
2022-05-21 19:00:54 -0500

    Add banner

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -4,3 +4,15 @@ const mainnav = document.querySelector('.navigation');
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive')
 }, false);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#year").textContent = new Date().getFullYear();
+    let lastModified = new Date(document.lastModified);
+    document.querySelector("#last-update").textContent = `${lastModified.getMonth()}/${lastModified.getDate()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()}`
+
+
+    let day = new Date().getDay();
+    if(day == 1 || day == 2) {
+        document.querySelector(".banner").style.display = "block";
+    }
+});
